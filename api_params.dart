a5f6a033272196ba33e16b9b5d19a65ae9f8789b
Austin <aripang.com@gmail.com>
2020-10-30 03:02:05 +0800

    init;

---
+++ api_params.dart
@@ -0,0 +1,105 @@
+// class ChatModal {
+//   int chatId;
+//   List<int> userIds;
+//   List<String> lastMessage;
+//   List<String> lastMessageTime;
+//   bool isAccpeted = false;
+//   bool isForCard = false;
+//   String cardNumber = '';
+//   List<int> numNnreadMessage = [];
+// }
+
+// class MessageModal {
+//   int messageId;
+//   int senderId;
+//   int receiverId;
+//   String message = '';
+//   String time = '';
+//   int type = 0;
+//   int numEmoji = 0;
+//   double sizeEmoji = 20;
+//   bool isOnlyEmoji = true;
+// }
+
+// class CommentModal {
+//   int commentId;
+//   int active;
+//   int userId;
+//   int targetId;
+//   String comment;
+//   String time;
+//   bool isGuest = false;
+//   bool isForPost = true;
+//   int numLike1 = 0;
+//   int numLike2 = 0;
+//   int numLike3 = 0;
+//   int numLike4 = 0;
+//   int numLike5 = 0;
+//   int numLike6 = 0;
+//   int numComment = 0;
+// }
+
+// class NotiModal {
+//   int notiId;
+//   int senderId;
+//   int receiverId;
+//   int type;
+//   String time;
+//   String optionVal;
+// }
+
+// class PostModal {
+//   int postId;
+//   int active;
+//   int userId;
+//   String feed = '';
+//   String time = '';
+//   bool isGuest = false;
+//   String postName = '';
+//   String optionVal = '';
+//   String locationAddress = '';
+//   String locationCoordinate = '';
+//   String city = '';
+//   int numLike1 = 0;
+//   int numLike2 = 0;
+//   int numLike3 = 0;
+//   int numLike4 = 0;
+//   int numLike5 = 0;
+//   int numLike6 = 0;
+//   int numComment = 0;
+// }
+
+// class User1Modal {
+//   int userId = 0;
+//   String name = '';
+//   String email = '';
+//   String password = '';
+//   String photo = '';
+//   String birthday = '';
+//   String sex = '';
+//   String avatarIndex = '';
+//   String bio = '';
+//   String cardNumber = '';
+//   String cardImg = '';
+//   int cardVerified = 0;
+//   String locationAddress = '';
+//   String locationCoordinate = '';
+//   String city = '';
+//   String deviceToken = '';
+//   bool isNotiFromNewPost = true;
+//   bool isNotiFromOther = true;
+//   bool isNotiFromComment = true;
+//   bool isNotiFromReaction = true;
+//   bool isNotiFromChat = true;
+//   List<dynamic> locationArray;
+//   String language = '';
+//   int active;
+//   String banReason = '';
+//   String banReply = '';
+//   int radiusAround = 100;
+//   String postAroundCenterCoordinate;
+//   bool isGuest = false;
+//   String guestName = '';
+//   int numNoti = 0;
+//   int numUnreadMessage = 0;
+// }
