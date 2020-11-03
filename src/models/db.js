6ba0362c49426ded0b81238a206e2ea6ff293b51
Austin <aripang.com@gmail.com>
2020-11-03 23:52:19 +0800

    env;

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config");
 
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-});
+let connection;
+try {
+  connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB,
+  });
+} catch (error) {
+  console.log('[DB Connect]', error.message);
+  process.exit(1);
+}
 
 module.exports = connection;
