afa7cd68453e020ed5b5c4676345974aee8de178
Austin <aripang.com@gmail.com>
2021-09-22 09:47:46 +0800

    match/area-users;

--- src/controllers/match.controller.js
+++ src/controllers/match.controller.js
@@ -1,22 +1,27 @@
-const { Validator } = require('node-input-validator');
-const models = require('../models');
+const { Validator } = require("node-input-validator");
+const models = require("../models");
 
-const {
-  IUser,
-} = require('../types');
+const { IUser } = require("../types");
 
 exports.getAreaUsers = ({ user_id, last_id = null, limit = 5 }) => {
-  return models.user.getById(user_id).then(user => {
-    const iUser = new IUser(user);
-    if (!iUser.location_area) throw new Error('Location area is unavailable!');
-    return models.user.getAreaUsers({
-      user_id,
-      limit,
-      last_id,
-      location_area: iUser.location_area,
+  return models.user.getById(user_id)
+    .then((me) => {
+      const iMe = new IUser(me);
+      if (!iMe.location_area) throw new Error("Location area is unavailable!");
+      return models.user.getAreaUsers({
+        user_id,
+        limit,
+        last_id,
+        location_area: iMe.location_area,
+      });
     })
-      .then(users => {
-        return users;
-      })
-  })
-}
+    .then(users => {
+      return Promise.all(users.map(async user => {
+        const iUser = new IUser(user);
+        const photos = await models.photo.getUserGallery(iUser.id);
+        const nUser = iUser.asNormal();
+        nUser.gallery = photos;
+        return nUser;
+      }))
+    });
+};
---
+++ src/types/IPhoto.js
@@ -0,0 +1,28 @@
+const TreflaModel = require('./_TreflaModel');
+const { timestamp } = require('../helpers/common.helpers');
+const { moduleExpression } = require('@babel/types');
+
+class IPhoto extends TreflaModel {
+  constructor(args) {
+    super(args);
+    this.defineProperties();
+    this.jsonFields = [];
+    for (const key in args) {
+      if (this[key] !== undefined) this[key] = args[key];
+    }
+    this.jsonify();
+  }
+
+  defineProperties() {
+    this.id = 0;
+    this.user_id = 0;
+    this.url = '';
+    this.type = 'normal';
+    this.ratio = '1';
+    this.thumbnail = '';
+    this.create_time = timestamp();
+    this.update_time = timestamp();
+  }
+}
+
+module.exports = IPhoto;
